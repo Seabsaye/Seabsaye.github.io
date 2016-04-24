e923b0cffb3597cea79da233c174f61d5f1d5a80
Sebastian <sebastian.kolosa@gmail.com>
2016-04-23 23:25:48 -0400

    add initial window resize js

--- JS/script.js
+++ JS/script.js
@@ -2,12 +2,44 @@ angular.module("navBarModule", [])
 
 .controller("navBarController", ["$scope", function($scope) {
 
-	
+	var windowWidth = $(window).width();
+
+	$(window).on("resize", function() {
+
+		if (windowWidth < 768) {
+
+			$("#navbar").css("background-color", "#FFFFFF");
+
+		} else {
+			return;
+		}
+
+	});
 
 }])
 
 .factory("navBarFactory", function() {
 
-	
-	
+	return {
+
+		setCollapsedNavCSS: function() {
+
+			var windowWidth = $(window).width();
+
+			$(window).on("resize", function() {
+
+				if (windowWidth < 768) {
+
+					log("test");
+
+				} else {
+					return;
+				}
+
+			});
+
+		}
+
+	};
+
 })

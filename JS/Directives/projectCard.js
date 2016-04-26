b03137e439be5cdf03be8410e76c31cfb0feade8
Sebastian <sebastian.kolosa@gmail.com>
2016-04-26 09:04:23 -0400

    add jquery color and button fx

--- JS/Directives/projectCard.js
+++ JS/Directives/projectCard.js
@@ -4,34 +4,44 @@ angular.module("cardModule", [])
 
 	$scope.arkParadigm = {
 
-		title: "Ark Paradigm", image: "Images/arkParadigmLogoTitleCopy.png", description: "Developed company's website from scratch using HTML5, CSS3, Angular Js, Jquery, MySql, and Python", button: "VIEW SITE", link: "http://arkParadigm.com"
+		title: "Ark Paradigm", image: "Images/arkParadigmLogoTitleCopy.png", description: "Developed company's website from scratch using HTML5, CSS3, Angular Js, Jquery, MySql, and Python", button: "VIEW SITE", link: "http://arkParadigm.com", buttonIndex: 0, imgStyle: "margin-top: 22%"
 
 	};
 
 	$scope.joinmi = {
 
-		title: "Joinmi", image: "Images/joinmiLogo.svg", description: "App that streamlines impomptue get togethers. Built using the phonegap framework at Hack the 6ix 2016.", button: "CHECK IT OUT", link: "http://devpost.com/software/joinmi"
+		title: "Joinmi", image: "Images/joinmiLogo.svg", description: "App that streamlines impomptue get togethers. Built using the phonegap framework at Hack the 6ix 2016.", button: "CHECK IT OUT", link: "http://devpost.com/software/joinmi", buttonIndex: 1, imgStyle: "margin-top: 12%"
 
 	}
 
 	$scope.pixelbot = {
 
-		title: "pixelbot", image: "", description: "startupstartupstartupstartupstartupstartup", button: "VISIT SITE", link: "http://www.hellopixelbot.com"
+		title: "pixelbot", image: "", description: "startupstartupstartupstartupstart upstartupstartupstartupstartupstartup startupstartup", button: "VISIT SITE", link: "http://www.hellopixelbot.com", buttonIndex: 2, imgStyle: "margin-top: 22%"
 
 	}
 
 	$scope.marvin = {
 	
-		title: "Marvin", image: "", description: "A web bot built as a personal assistant to manage the schedules of all within a household. Developed with the power of regular expressions and Microsoft's Azure framework at mcHacks 2016.", button: "CHECK IT OUT"
+		title: "Marvin", image: "", description: "A web bot built as a personal assistant to manage the schedules of all within a household. Developed with the power of regular expressions and Microsoft's Azure framework at mcHacks 2016.", button: "CHECK IT OUT", buttonIndex: 3, imgStyle: "margin-top: 22%"
 	
 	};
 
-	$scope.buttonOnHover = function(button) {
+	$scope.projects = [$scope.arkParadigm, $scope.joinmi, $scope.pixelbot, $scope.marvin];
+
+	var cardButtonInstances = document.getElementsByClassName("cardButton");
+
+	$scope.buttonOnHover = function(buttonIndex) {
+
+		var button = cardButtonInstances[buttonIndex];
 		cardButtonFactory.upTheOpacity(button);
+
 	};
 
-	$scope.buttonOffHover = function(button) {
+	$scope.buttonOffHover = function(buttonIndex) {
+
+		var button = cardButtonInstances[buttonIndex];
 		cardButtonFactory.downTheOpacity(button);
+
 	};
 
 }])
@@ -55,15 +65,11 @@ angular.module("cardModule", [])
 	return {
 
 		upTheOpacity: function(element) {
-			
-			$(element).animate({opacity: '1.0'}, 2000);
-
+			$(element).stop(true).animate({backgroundColor: jQuery.Color({alpha: "0.04"}) }, 225);
 		},
 
 		downTheOpacity: function(element) {
-
-			$(element).animate({opacity: '0.0'}, 2000);
-
+			$(element).stop(true).animate({backgroundColor: jQuery.Color({alpha: "0.00"}) }, 225);
 		}
 
 	}

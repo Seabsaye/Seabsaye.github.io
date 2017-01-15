31f5b52d6fe24ef7481a688a5c36ffbed5498b94
Sebastian <sebastian.kolosa@gmail.com>
2017-01-14 23:48:45 -0500

    fix project card positioning bug and add everest project card

--- JS/Directives/projectCard.js
+++ JS/Directives/projectCard.js
@@ -2,47 +2,57 @@ angular.module("cardModule", ["pageLayoutModule"])
 
 .controller("cardController", ["$scope", "buttonFactory", function($scope, buttonFactory) {
 
+	$scope.everest = {
+		title: "Everest",
+		image: "Images/everestLogo.png",
+		description: "iOS app that optimizes event communication and organization via in-event newsfeeds, direct messaging, lists of guests and hosts, and more. Built using Swift coupled with a NodeJS and MongoDB backend.",
+		button: "GET IT",
+		link: "https://itunes.apple.com/ca/app/family-allowance/id1090612479?mt=8",
+		buttonIndex: 0,
+		imgStyle: "margin-top: 15%; height: 140px"
+	};
+
 	$scope.td = {
 		title: "TD Innovation Labs", 
 		image: "Images/tdBankLogo.png", 
-		description: "Architected a child savings breakdown feature in TD Family Allowance using Objective-C, and built a new, confidential Swift app to the MVP", 
-		button: "CHECK IT OUT", 
-		buttonIndex: 0,
+		description: "Built a significant, currently confidential Swift app to the MVP, and architected features such as child savings breakdown in the TD Family Allowance Objective-C app.", 
+		button: "CHECK IT OUT",
 		link: "https://itunes.apple.com/ca/app/family-allowance/id1090612479?mt=8",
-		imgStyle: "margin-top: 12%; height: 140px"
+		buttonIndex: 1,
+		imgStyle: "margin-top: 15%; height: 140px"
 	};
 
 	$scope.arkParadigm = {
 		title: "Ark Paradigm", 
 		image: "Images/arkParadigmLogoTitle.png", 
-		description: "Developed startup's website using HTML5, CSS3, AngularJS, jQuery, MySQL, and Python.", 
+		description: "Implemented startup's website using HTML5, CSS3, AngularJS 1, jQuery, MySQL, and Python.", 
 		button: "CHECK IT OUT", 
 		link: "http://arkParadigm.com", 
-		buttonIndex: 1, 
+		buttonIndex: 2, 
 		imgStyle: "margin-top: 22%"
 	};
 
 	$scope.joinmi = {
 		title: "Joinmi", 
 		image: "Images/joinmiLogo.svg", 
-		description: "App that streamlines impromptu get togethers. Built using the Phonegap framework at Hack the 6ix 2016.", 
+		description: "App that streamlines impromptu get togethers. Built using HTML5, CSS3 and JavaScript with the Phonegap framework at Hack the 6ix 2016.", 
 		button: "CHECK IT OUT", 
 		link: "http://devpost.com/software/joinmi", 
-		buttonIndex: 2, 
+		buttonIndex: 3, 
 		imgStyle: "margin-top: 12%"
 	};
 
 	$scope.pixelbot = {
 		title: "Pixelbot", 
 		image: "Images/pixelbotLogo.png", 
-		description: "Built \"Uber for condos\" iOS app beta for startup using Swift, PHP, and MySQL.", 
+		description: "Developed \"Uber for condos\" iOS app for startup using Swift, PHP, and MySQL, as well as integrating Facebook, Google, and native login and account-merging systems.", 
 		button: "CHECK IT OUT", 
 		link: "https://github.com/Seabsaye/iOS-Project-Alfred", 
-		buttonIndex: 3, 
+		buttonIndex: 4, 
 		imgStyle: "margin-top: 15%; height: 140px"
 	};
 
-	$scope.projects = [$scope.td, $scope.arkParadigm, $scope.joinmi, $scope.pixelbot];
+	$scope.projects = [$scope.everest, $scope.td, $scope.arkParadigm, $scope.joinmi, $scope.pixelbot];
 
 	var cardButtonInstances = document.getElementsByClassName("cardButton");
 

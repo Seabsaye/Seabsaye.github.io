da02c8780a6a32df0a4f502cad8462281b74cd3d
Sebastian <sebastian.kolosa@gmail.com>
2017-09-23 23:41:43 -0400

    Add TribalScale card and cleanup code

--- JS/Directives/projectCard.js
+++ JS/Directives/projectCard.js
@@ -5,8 +5,11 @@ angular.module("cardModule", ["pageLayoutModule"])
 	$scope.everest = {
 		title: "Everest",
 		image: "Images/everestLogo.png",
-		description: "iOS app that optimizes event communication and organization via in-event newsfeeds, direct messaging, lists of guests and hosts, and more. Built using Swift, NodeJS, and MongoDB coupled with AWS EC2.",
-		button: "COMPANY WEBSITE",
+		description: "iOS app that optimizes event communication and organization via " + 
+					 "in-event newsfeeds, direct messaging, lists of guests and hosts, " +
+					 "and more. Being built using Swift, Node.js, and MongoDB coupled " +
+					 "with AWS EC2.",
+		button: "CHECK IT OUT",
 		link: "http://www.everestapp.ca",
 		buttonIndex: 0,
 		imgStyle: "margin-top: 15%; height: 140px",
@@ -16,8 +19,10 @@ angular.module("cardModule", ["pageLayoutModule"])
 	$scope.tribalscale = {
 		title: "TribalScale",
 		image: "Images/tribalscaleLogo.png",
-		description: "Architected cruise guest organizer user interface for major cruise line's Objective-C iOS app used by 20k+ cruise guests daily.",
-		button: "CHECK IT OUT",
+		description: "Architected event organizer for cruise passengers in major cruise " +
+					 "line's Objective-C iOS app used by 20k+ passengers daily. Developed " +
+					 "this feature and many others in an Extreme Programming environment.",
+		button: "COMPANY SITE",
 		link: "http://www.tribalscale.com",
 		buttonIndex: 1,
 		imgStyle: "margin-top: 15%; height: 140px",
@@ -27,18 +32,21 @@ angular.module("cardModule", ["pageLayoutModule"])
 	$scope.td = {
 		title: "TD Innovation Labs", 
 		image: "Images/tdBankLogo.png", 
-		description: "Built a significant, currently confidential Swift app to the MVP, and architected features such as child savings breakdown in the TD Family Allowance Objective-C app.", 
-		button: "GET IT",
-		link: "https://itunes.apple.com/ca/app/family-allowance/id1090612479?mt=8",
+		description: "Built a significant, currently confidential Swift app to the MVP. " +
+					  "Architected features such as child savings breakdown and Touch ID " +
+					  "authentication for the TD Family Allowance Objective-C app.", 
+		button: "CHECK IT OUT",
+		link: "https://appadvice.com/app/family-allowance/1090612479",
 		buttonIndex: 2,
 		imgStyle: "margin-top: 15%; height: 140px",
-		imageLink: "https://itunes.apple.com/ca/app/family-allowance/id1090612479?mt=8"
+		imageLink: "https://appadvice.com/app/family-allowance/1090612479"
 	};
 
 	$scope.arkParadigm = {
 		title: "Ark Paradigm", 
 		image: "Images/arkParadigmLogoTitle.png", 
-		description: "Implemented startup's website using HTML5, CSS3, AngularJS 1, jQuery, MySQL, and Python.", 
+		description: "Implemented startup's website using HTML5, CSS3, AngularJS 1, " +
+			         "jQuery, MySQL, and Python.", 
 		button: "CHECK IT OUT", 
 		link: "http://arkParadigm.com", 
 		buttonIndex: 3, 
@@ -49,7 +57,9 @@ angular.module("cardModule", ["pageLayoutModule"])
 	$scope.joinmi = {
 		title: "Joinmi", 
 		image: "Images/joinmiLogo.svg", 
-		description: "App that streamlines impromptu get togethers. Built using HTML5, CSS3 and JavaScript with the Phonegap framework at Hack the 6ix 2016.", 
+		description: "App that streamlines impromptu get togethers. Built using " +
+		 			 "HTML5, CSS3 and JavaScript with the Phonegap framework at " +
+		 			 "Hack the 6ix 2016.", 
 		button: "CHECK IT OUT", 
 		link: "http://devpost.com/software/joinmi", 
 		buttonIndex: 4, 
@@ -60,7 +70,9 @@ angular.module("cardModule", ["pageLayoutModule"])
 	$scope.pixelbot = {
 		title: "Pixelbot", 
 		image: "Images/pixelbotLogo.png", 
-		description: "Developed \"Uber for condos\" iOS app for startup using Swift, PHP, and MySQL, as well as integrating Facebook, Google, and native login and account-merging systems.", 
+		description: "Developed features for startup's \"Uber for condos\" iOS app using Swift, " +
+					 "PHP, and MySQL. Created Facebook, Google, and native account login and " +
+					 "an account data merging system.", 
 		button: "CHECK IT OUT", 
 		link: "https://github.com/Seabsaye/iOS-Project-Alfred", 
 		buttonIndex: 5, 
@@ -68,7 +80,7 @@ angular.module("cardModule", ["pageLayoutModule"])
 		imageLink: "http://www.hellopixelbot.com"
 	};
 
-	$scope.projects = [$scope.everest, $scope.td, $scope.arkParadigm, $scope.joinmi, $scope.pixelbot];
+	$scope.projects = [$scope.everest, $scope.tribalscale, $scope.td, $scope.arkParadigm, $scope.joinmi, $scope.pixelbot];
 
 	var cardButtonInstances = document.getElementsByClassName("cardButton");
 
